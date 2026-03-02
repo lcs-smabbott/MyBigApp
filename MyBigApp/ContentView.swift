9297c74d1592be156248b2cb492c8fd688ee38ef
Sadie Mabbott <smabbott@lcs.on.ca>
2026-03-02 12:22:17 -0500

    Created the rectangle for a picutre and the home button.

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -9,16 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Rectangle()
+                    .frame(width: 250, height: 250)
+                Spacer()
+                VStack {
+                    Text("Blue")
+                        .font(.system(.largeTitle, design: .default, weight: .bold))
+                    Text("Joni Mitchell")
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Text("Release date")
+                                .bold()
+                            Text("June 22, 1971")
+                        }
+                    }
+                }
+            }
+                .toolbar {
+                    
+                    ToolbarItem(placement: .primaryAction) {
+                        
+                        Button {
+                            //doesn't do anything yet
+                        } label: {
+                            Image(systemName: "house")
+                    
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
-}
 
+
+        
 #Preview {
     ContentView()
 }
